e2f766eafa786a4d8c032f24ec886e3f7348797c
Andrew Torr <andrewtorr@andrews-air.home>
2022-03-29 12:13:17 +0100

    GET /api/topics complete

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const { getTopics } = require("./controllers/controller");
+
+const {
+  handleCustomErrors,
+  handlePsqlErrors,
+  handleServerErrors,
+} = require("./errors/index");
+
+const app = express();
+
+app.use(express.json());
+
+//api requests
+app.get("/api/topics", getTopics);
+
+//error handling
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
+app.use(handleServerErrors);
+
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "Route not found" });
+});
+
+module.exports = app;
