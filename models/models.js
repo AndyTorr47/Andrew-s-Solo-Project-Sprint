4bfced6b3f23175c86250a8a3ddc7b319f44d8ed
Andrew Torr <andrewtorr@andrews-air.home>
2022-03-30 12:39:56 +0100

    PATCH /api/articles/:article_id complete

--- models/models.js
+++ models/models.js
@@ -11,3 +11,16 @@ exports.selectArticles = () => {
     return result.rows;
   });
 };
+
+exports.patchArticle = (article_id, inc_votes) => {
+  return db
+    .query(
+      `UPDATE articles SET votes = votes + ${inc_votes} WHERE article_id = ${article_id} RETURNING *;`
+    )
+    .then((result) => {
+      if (!result.rows.length) {
+        return Promise.reject({ msg: "Route not found", status: 404 });
+      }
+      return result.rows[0];
+    });
+};
